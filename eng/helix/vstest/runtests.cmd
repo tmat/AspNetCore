174d56c8aa20b6f826d5b2521bc363a064f98e17
Hao Kung <HaoK@users.noreply.github.com>
2019-02-20 19:43:11 -0800

    Fix path

--- eng/helix/vstest/runtests.cmd
+++ eng/helix/vstest/runtests.cmd
@@ -5,7 +5,7 @@ powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePo
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Runtime dotnet -Version %runtimeVersion% -InstallDir %HELIX_CORRELATION_PAYLOAD%\sdk"
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%/sdk
-set PATH=$DOTNET_ROOT:$PATH
+set PATH=%DOTNET_ROOT%;%PATH%
 set DOTNET_MULTILEVEL_LOOKUP=0
 set DOTNET_CLI_HOME=%HELIX_CORRELATION_PAYLOAD%/home
 set helix=true
